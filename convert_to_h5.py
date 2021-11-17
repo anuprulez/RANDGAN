2953cbddc84bc5e49f99e30e246dbd15e03fba31
Anup Kumar <anup.rulez@gmail.com>
2021-11-17 10:23:24 +0100

    update

--- convert_to_h5.py
+++ convert_to_h5.py
@@ -7,9 +7,11 @@ import os
 base_path = "/data/users/backup/anup/randgan/create_randgan_datasets/data/"
 #base_path = "/home/anupkumar/randgan/RANDGAN/RANDGAN/"
 train_path = base_path + "train/"
+train_pred_path = base_path + "train_pred/"
 test_path = base_path + "test/"
 test_pred_path = base_path + "test_pred/"
 tr_h5 = "train_images_negative_txt_file.h5"
+tr_pred_h5 = "train_images_negative_txt_file_pred.h5"
 te_h5 = "te_images_all_txt_file.h5"
 te_pred_h5 = "te_pred_images_all_txt_file.h5"
 
@@ -34,7 +36,7 @@ def convert_hdf5_train(files, image_path, output_path, key):
                 img = np.asarray(img.resize((256, 256)))
                 l_images.append(img)
                 ctr += 1
-                print("Image processed: {}".format(str(ctr)))
+                #print("Image processed: {}".format(str(ctr)))
             except:
                 continue
     l_images = np.array(l_images)
@@ -55,7 +57,7 @@ def convert_hdf5_test(files, image_path, output_path, key):
             img = np.asarray(img.resize((256, 256)))
             l_images.append(img)
             ctr += 1
-            print("Image processed: {}".format(str(ctr)))
+            #print("Image processed: {}".format(str(ctr)))
         except:
             continue
     l_images = np.array(l_images)
@@ -67,8 +69,11 @@ def convert_hdf5_test(files, image_path, output_path, key):
 print("Saving training images...")
 convert_hdf5_train(read_text_file("train_split.txt"), train_path, tr_h5, "tr_images")
 
+print("Saving training pred/segmented images...")
+convert_hdf5_train(read_text_file("train_split.txt"), train_pred_path, tr_pred_h5, "tr_pred_images")
+
 print("Saving test images...")
 convert_hdf5_test(read_text_file("test_split.txt"), test_path, te_h5, "te_images")
 
-print("Saving test pred images...")
+print("Saving test pred/segmented images...")
 convert_hdf5_test(read_text_file("test_split.txt"), test_pred_path, te_pred_h5, "te_pred_images")
